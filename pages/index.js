61ea57ecfc159ed17e8fee1496485a5d9c0e24b7
Fernando Pena <nando.pena@gmail.com>
2021-01-25 23:10:13 -0300

    Alterar nome da pasta QuizLogo

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import db from '../db.json';
 import Widget from '../src/components/Widget'
 import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
+import QuizBackground from '../src/components/QuizBackground' //Teste
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
 
